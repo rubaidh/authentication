7898d025f56620e8e2e0cb97030581709ae2c512
Mark Connell <mark@rubaidh.com>
2009-05-06 17:18:18 +0100

    add default_url_option to the test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,7 +22,6 @@ config.action_controller.allow_forgery_protection    = false
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
-# Use SQL instead of Active Record's schema dumper when creating the test database.
-# This is necessary if your schema can't be completely dumped by the schema dumper,
-# like if you have constraints or database-specific column types
-# config.active_record.schema_format = :sql
+config.action_mailer.default_url_options = {
+  :host => 'test.host'
+}
