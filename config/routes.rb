033b3d8846c627582427027d8ba1583396e5037a
Mark Connell <mark@rubaidh.com>
2009-05-06 11:02:08 +0100

    Copy in the existing routes for the authentication

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+ActionController::Routing::Routes.draw do |map|
+
+  map.resource :user
+  map.resource :password, :only => [:new, :create, :edit, :update]
+  map.resource :activation, :only => [:new, :create]
+  map.resource :session
+
+  map.namespace :admin do |admin|
+    admin.resources :users, :member     => { :activate => :put, :suspend => :put, :reset_password => :put },
+                            :collection => { :inactive => :get }
+  end
+
+  map.login   "/login",  :controller => 'sessions', :action => 'new'
+  map.logout  "/logout", :controller => 'sessions', :action => 'destroy'
+  map.signup  "/signup", :controller => 'users',     :action => 'new'
+
+  map.root :controller => 'sessions', :action => 'new'
+  map.activate "/login/activate/:activation_code", :controller => "logins", :action => "activate", :activation_code => nil
+
+end
