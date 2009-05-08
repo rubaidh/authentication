f839b8ace65862f9961d161a3523ff80569d0097
Mark Connell <mark@rubaidh.com>
2009-05-08 14:58:17 +0100

    Add authentication models generator to the plugin

--- generators/authentication/authentication_generator.rb
+++ generators/authentication/authentication_generator.rb
@@ -3,6 +3,7 @@ class AuthenticationGenerator < Rails::Generator::Base
     record do |m|
       m.dependency 'authentication_features', [], :collision => :skip
       m.dependency 'authentication_migrations', [], :collision => :skip
+      m.dependency 'authentication_models', [], :collision => :skip
       m.dependency 'authentication_assets', [], :collision => :skip
     end
   end
