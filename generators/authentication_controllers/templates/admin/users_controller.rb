595e5a8eeabf849c0405b3f97a6ac9c6e544ae18
Mark Connell <mark@rubaidh.com>
2009-05-08 11:02:46 +0100

    Nuke the authentication_controllers generator because we don't really need it

--- generators/authentication_controllers/templates/admin/users_controller.rb
+++
@@ -1,4 +0,0 @@
-# I'm pre-implementated by Rubaidh::Authentication but if you want to override the behaviour
-# just add it to this class
-class Admin::UsersController < Rubaidh::Authentication::Admin::UsersController
-end
--- generators/authentication_controllers/templates/sessions_controller.rb
+++
@@ -1,4 +0,0 @@
-# I'm pre-implementated by Rubaidh::Authentication but if you want to override the behaviour
-# just add it to this class
-class SessionsController < Rubaidh::Authentication::SessionsController
-end
