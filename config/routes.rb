d0eef8faee9ea135a9f871031198b4fcd01abe2e
Mark Connell <mark@rubaidh.com>
2009-05-13 16:07:40 +0100

    Add routing for the new action

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :session
 
   map.namespace :admin do |admin|
-    admin.resources :users, :member     => { :activate => :put, :suspend => :put, :reset_password => :put },
+    admin.resources :users, :member     => { :activate => :put, :suspend => :put, :reset_password => :put, :administrator => :put },
                             :collection => { :inactive => :get }
   end
 
