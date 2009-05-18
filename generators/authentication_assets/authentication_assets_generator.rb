ad38fbdb6584722c5c84e5828f13328567dee3b2
Mark Connell <mark@rubaidh.com>
2009-05-18 16:51:49 +0100

    Add a 403 error file to the generated assets

--- generators/authentication_assets/authentication_assets_generator.rb
+++ generators/authentication_assets/authentication_assets_generator.rb
@@ -3,6 +3,7 @@ class AuthenticationAssetsGenerator < Rails::Generator::Base
     record do |m|
       m.directory(File.join('public', 'stylesheets'))
 
+      m.file('403.html', File.join('public', '403.html'))
       m.file(File.join('stylesheets', 'base.css'), File.join('public', 'stylesheets', 'base.css'))
       m.file(File.join('stylesheets', 'style.css'), File.join('public', 'stylesheets', 'style.css'))
     end
