e4898eca9ea4f525e021ddf3c742fbfabf971853
Mark Connell <mark@rubaidh.com>
2009-06-27 17:42:43 +0100

    Alter the features generator (since it has another generation related to rspec already) so that it correctly adds in additional methods to spec_helper that allow our specs to make user of login and login :administrator

--- generators/authentication_features/authentication_features_generator.rb
+++ generators/authentication_features/authentication_features_generator.rb
@@ -23,6 +23,25 @@ class AuthenticationFeaturesGenerator < Rails::Generator::Base
       # we use object_daddy for both features and specs and it is probably a good idea to give the application a copy of them
       m.directory(File.join('spec', 'exemplars'))
       m.file(File.join('..', '..', '..', 'spec', 'exemplars', 'user_exemplar.rb'), File.join('spec', 'exemplars', 'user_exemplar.rb'))
+
+
+      ## generate spec_helper changes
+      ## strictly this doesn't belong in this generator but seems the most appropriate without creating an additional
+      m.gsub_file File.join('spec', 'spec_helper.rb'), /require \'spec\/rails\'/ do |match|
+<<-STR
+#{match}
+
+def test_login_as(role = nil)
+  user = (role.is_a?(Symbol) ? generate_user(role => true) : generate_user)
+  controller.send :current_user=, user
+end
+alias :login :test_login_as
+
+def generate_user(stubs = {})
+  User.generate(stubs)
+end
+STR
+      end
     end
   end
 end
