5680e9ffe01d0c9e9180232cba75de22eb8f3c0e
Mark Connell <mark@rubaidh.com>
2009-06-27 16:13:51 +0100

    Add an additional generator to the plugin that will copy across the default layout and user_mailer templates

--- generators/authentication/authentication_generator.rb
+++ generators/authentication/authentication_generator.rb
@@ -1,10 +1,11 @@
 class AuthenticationGenerator < Rails::Generator::Base
   def manifest
     record do |m|
-      m.dependency 'authentication_features', [], :collision => :skip
+      m.dependency 'authentication_features',   [], :collision => :skip
       m.dependency 'authentication_migrations', [], :collision => :skip
-      m.dependency 'authentication_models', [], :collision => :skip
-      m.dependency 'authentication_assets', [], :collision => :skip
+      m.dependency 'authentication_models',     [], :collision => :skip
+      m.dependency 'authentication_assets',     [], :collision => :skip
+      m.dependency 'authetnication_templates',  [], :collision => :skip
     end
   end
 end
